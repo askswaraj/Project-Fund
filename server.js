b8c1a2ee06872eeffaf7ff6c06b6cad6a143694f
Abhishek Swaraj <abhishektheswaraj@hotmail.com>
2022-01-28 18:19:05 +0530

    v7.0

--- server.js
+++ server.js
@@ -16,11 +16,3 @@ app.prepare().then(()=>{
         console.log('Ready on local host:3000')
     })
 })
-if(process.env.NODE_ENV === 'production'){    
-    app.use(express.static('frontend/build'))  // set static folder 
-    //returning frontend for any route other than api 
-    app.get('*',(req,res)=>{     
-        res.sendFile (path.resolve(__dirname,'frontend','build',         
-                      'index.html' ));    
-    });
-}
